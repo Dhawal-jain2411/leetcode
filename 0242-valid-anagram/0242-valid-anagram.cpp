bab5eb43a19cf309b72c45e1621446bd83c7527c
Dhawal-jain2411 <jdhawal2411@gmail.com>
2026-06-22 18:04:38 +0530

    Time: 7 ms (19.14%), Space: 9.8 MB (49.38%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+      bool anagram = true;
+      sort(s.begin(),s.end());
+      sort(t.begin(),t.end());
+      
+        for(int i =0;i<s.length();i++){
+            if (s[i]!=t[i]){
+                anagram= false;
+            }
+
+        }
+
+        return anagram ; 
+    }
+};
