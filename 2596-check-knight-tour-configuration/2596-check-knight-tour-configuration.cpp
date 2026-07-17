079e09d55172fd6aededc83377977e674381e42c
Dhawal-jain2411 <jdhawal2411@gmail.com>
2026-07-18 02:45:46 +0530

    Time: 0 ms (100%), Space: 31.8 MB (66.14%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
@@ -1,41 +1,89 @@
 class Solution {
 public:
+    bool check(vector<vector<int>>& grid, int row, int col, int& nr, int& nc,
+               int currnum, int n) {
+        
+        if (row >= 1 && col >= 2) {
+            if (grid[row - 1][col - 2] == currnum + 1) {
+                nr = row - 1;
+                nc = col - 2;
+                return true;
+            }
+        }
+        if (row >= 2 && col >= 1) {
+            if (grid[row - 2][col - 1] == currnum + 1) {
+                nr = row - 2;
+                nc = col - 1;
+                return true;
+            }
+        }
+        if (row >= 2 && col < n - 1) {
+            if (grid[row - 2][col + 1] == currnum + 1) {
+                nr = row - 2;
+                nc = col + 1;
+                return true;
+            }
+        }
+        if (row >= 1 && col < n - 2) {
+            if (grid[row - 1][col + 2] == currnum + 1) {
+                nr = row - 1;
+                nc = col + 2;
+                return true;
+            }
+        }
+        if (row < n - 1 && col < n - 2) {
+            if (grid[row + 1][col + 2] == currnum + 1) {
+                nr = row + 1;
+                nc = col + 2;
+                return true;
+            }
+        }
+        if (row < n - 2 && col < n - 1) {
+            if (grid[row + 2][col + 1] == currnum + 1) {
+                nr = row + 2;
+                nc = col + 1;
+                return true;
+            }
+        }
+        if (row < n - 2 && col >= 1) {
+            if (grid[row + 2][col - 1] == currnum + 1) {
+                nr = row + 2;
+                nc = col - 1;
+                return true;
+            }
+        }
+        if (row < n - 1 && col >= 2) {
+            if (grid[row + 1][col - 2] == currnum + 1) {
+                nr = row + 1;
+                nc = col - 2;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    bool travel(vector<vector<int>>& grid, int row, int col, int n,
+                int currnum) {
+        if (currnum == n * n - 1)
+            return true;
+            
+        int nr = -1, nc = -1;
+        
+        if (check(grid, row, col, nr, nc, currnum, n)) {
+            if (travel(grid, nr, nc, n, currnum + 1)) {
+                return true;
+            }
+            return false; 
+        } else {
+            return false;
+        }
+    }
+    
     bool checkValidGrid(vector<vector<int>>& grid) {
-         if (grid[0][0] != 0) return false;
+        if (grid[0][0] != 0) return false;
         
         int n = grid.size();
         
-         int dr[] = {-2, -2, -1, -1, 1, 1, 2, 2};
-        int dc[] = {-1, 1, -2, 2, -2, 2, -1, 1};
-        
-        int row = 0;
-        int col = 0;
-        int expectedNum = 1;
-        int target = n * n;
-        
-         while (expectedNum < target) {
-            bool foundNext = false;
-            
-             for (int i = 0; i < 8; i++) {
-                int nextRow = row + dr[i];
-                int nextCol = col + dc[i];
-                
-                 if (nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < n) {
-                    if (grid[nextRow][nextCol] == expectedNum) {
-                        row = nextRow;
-                        col = nextCol;
-                        expectedNum++;
-                        foundNext = true;
-                        break; 
-                    }
-                }
-            }
-            
-             if (!foundNext) {
-                return false;
-            }
-        }
-        
-        return true;
+        return travel(grid, 0, 0, n, 0);
     }
 };
